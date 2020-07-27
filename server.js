7647fb7c6f6dddaf224aac6617bc8f0ab5000c35
weilyuwang <weilyuwang@gmail.com>
2020-07-27 14:15:41 -0400

    :art: color console

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
 const connectDB = require("./config/db");
 
 // Load env vars
@@ -27,12 +28,16 @@ app.use("/api/v1/bootcamps", bootcampsRouter);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 // Handle unhandled promise rejections
+// The 'unhandledRejection' event is emitted whenever a Promise is rejected and
+// no error handler is attached to the promise within a turn of the event loop.
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   // Close server and exit process
   server.close(() => process.exit(1));
 });
