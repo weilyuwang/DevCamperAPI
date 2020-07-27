29d656bd8afc8fb7cfe8e90a95e0f63e59668ce1
weilyuwang <weilyuwang@gmail.com>
2020-07-27 14:11:13 -0400

    :bug: handle rejected promises

--- server.js
+++ server.js
@@ -26,6 +26,13 @@ app.use("/api/v1/bootcamps", bootcampsRouter);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
