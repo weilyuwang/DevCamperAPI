8b727ad5cbb50888d8f5c479d4ed8597a3587a94
weilyuwang <weilyuwang@gmail.com>
2020-07-27 20:52:30 -0400

    :fire: get bootcamp(s)

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,17 +3,30 @@ const Bootcamp = require("../models/Bootcamp");
 // @desc      Get all bootcamps
 // @route     GET /api/v1/bootcamps
 // @access    Public
-const getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+const getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc      Get a single bootcamp
 // @route     GET /api/v1/bootcamps/:id
 // @access    Public
-const getBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Display bootcamp ${req.params.id}` });
+const getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc      Create new bootcamp
