b13d4f88b50cffaea81a8d1612d4379f97b64b79
weilyuwang <weilyuwang@gmail.com>
2020-07-28 10:56:38 -0400

    :zap: geocoder lib and pre save hook

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const geocoder = require("../utils/geocoder");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -45,7 +46,6 @@ const BootcampSchema = new mongoose.Schema({
     },
     coordinates: {
       type: [Number],
-
       index: "2dsphere",
     },
     formattedAddress: String,
@@ -102,6 +102,26 @@ const BootcampSchema = new mongoose.Schema({
 // Create bootcamnp slug from the name
 BootcampSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode & create location field
+BootcampSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  // Do not save address in DB
+  this.address = undefined;
+
   next();
 });
 
