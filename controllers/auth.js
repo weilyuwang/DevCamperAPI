7331ee71e886d2201de956ea721f78b8e4c49e19
weilyuwang <weilyuwang@gmail.com>
2020-08-17 11:30:00 -0400

    :fire: send JWT in cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,10 +14,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     name, email, password, role
   })
 
-  // Create JWT 
-  const token = user.getSignedJwtToken()
-
-  res.status(200).json({ success: true, token: token })
+  sendTokenResponse(user, 200, res)
 })
 
 
@@ -50,9 +47,28 @@ exports.login = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse('Invalid credentials', 401))
   }
 
+  sendTokenResponse(user, 200, res)
+})
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
   // Create JWT 
   const token = user.getSignedJwtToken()
 
-  res.status(200).json({ success: true, token: token })
-})
+  // token MUST be stored in the client’s cookies with flag HTTP Only 
+  // so it can’t be accessed from scripts (XSS).
+  const options = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+    httpOnly: true
+  }
 
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true
+  }
+
+  res
+    .status(statusCode)
+    .cookie('token', token, options)
+    .json({ success: true, token: token })
+
+}
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
 const fileupload = require("express-fileupload")
+const cookieParser = require('cookie-parser')
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 const path = require('path')
@@ -24,6 +25,9 @@ const app = express();
 
 // JSON Body Parser
 app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser())
 
 // Dev logging middleware
 if (process.env.NODE_ENV === "development") {
