fb9fa764503de2f13fc9ec091af2a0043818a32f
weilyuwang <weilyuwang@gmail.com>
2020-08-17 12:27:46 -0400

    :fire: role auth middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -17,7 +17,7 @@ const courseRouter = require("./courses");
 
 const router = express.Router();
 
-const { protect } = require('../middleware/auth')
+const { protect, authorize } = require('../middleware/auth')
 
 // Re-route into other resource routers
 // Re-route /api/v1/bootcamps/:bootcampId/courses to /api/v1/courses with params merged
@@ -25,14 +25,14 @@ router.use("/:bootcampId/courses", courseRouter);
 
 router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 
-router.route("/").get(advancedResults(Bootcamp, "courses"), getBootcamps).post(protect, createBootcamp);
+router.route("/").get(advancedResults(Bootcamp, "courses"), getBootcamps).post(protect, authorize('publisher', 'admin'), createBootcamp);
 
-router.route("/:id/photo").put(protect, bootcampPhotoUpload)
+router.route("/:id/photo").put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload)
 
 router
   .route("/:id")
   .get(getBootcamp)
-  .put(protect, updateBootcamp)
-  .delete(protect, deleteBootcamp);
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
 module.exports = router;
--- routes/courses.js
+++ routes/courses.js
@@ -9,7 +9,7 @@ const {
 
 const Course = require('../models/Course')
 const advancedResults = require('../middleware/advancedResults')
-const { protect } = require('../middleware/auth')
+const { protect, authorize } = require('../middleware/auth')
 
 const router = express.Router({ mergeParams: true });
 
@@ -19,8 +19,11 @@ router.route("/")
     select: "name description",
   }),
     getCourses)
-  .post(protect, addCourse);
+  .post(protect, authorize('publisher', 'admin'), addCourse);
 
-router.route("/:id").get(getCourse).put(protect, updateCourse).delete(protect, deleteCourse);
+router.route("/:id")
+  .get(getCourse)
+  .put(protect, authorize('publisher', 'admin'), updateCourse)
+  .delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 module.exports = router;
