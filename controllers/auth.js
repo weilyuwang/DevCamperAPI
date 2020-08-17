debb07edaa8f952cd1574229395e4b89e9a2b459
weilyuwang <weilyuwang@gmail.com>
2020-08-17 15:57:22 -0400

    :fire: generate reset password token

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,7 +18,6 @@ exports.register = asyncHandler(async (req, res, next) => {
 })
 
 
-
 // @desc      Login User
 // @route     POST /api/v1/auth/login
 // @access    Public
@@ -50,7 +49,53 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res)
 })
 
-// Get token from model, create cookie and send response
+
+// @desc      Get current logged in user
+// @route     GET /api/v1/auth/me
+// @access    Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id)
+
+  res.status(200).json({
+    success: true,
+    data: user
+  })
+})
+
+
+
+
+// @desc      Forgot password
+// @route     POST /api/v1/auth/forgotpassword
+// @access    Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email })
+
+  if (!user) {
+    return next(new ErrorResponse('There is no user with that email', 404))
+  }
+
+  // Get reset token
+  const resetToken = user.getResetPasswordToken()
+
+  await user.save({ validateBeforeSave: false })
+
+  res.status(200).json({
+    success: true,
+    data: user
+  })
+})
+
+
+
+
+
+
+
+
+
+
+// @desc   Reusable helper function to get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create JWT 
   const token = user.getSignedJwtToken()
@@ -72,17 +117,3 @@ const sendTokenResponse = (user, statusCode, res) => {
     .json({ success: true, token: token })
 
 }
-
-
-
-// @desc      Get current logged in user
-// @route     GET /api/v1/auth/me
-// @access    Private
-exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id)
-
-  res.status(200).json({
-    success: true,
-    data: user
-  })
-})
