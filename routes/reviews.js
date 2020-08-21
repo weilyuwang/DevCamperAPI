828b242e9a5ecd49704d83bb660bdd34061afdb9
weilyuwang <weilyuwang@gmail.com>
2020-08-20 21:14:12 -0400

    :fire: create review for bootcamp

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const {
   getReviews,
-  getReview
+  getReview,
+  addReview
 } = require("../controllers/reviews");
 
 const Review = require('../models/Review')
@@ -16,6 +17,7 @@ router.route("/")
     path: "bootcamp",
     select: "name description",
   }), getReviews)
+  .post(protect, authorize('user', 'admin'), addReview)
 
 router.route('/:id').get(getReview)
 
