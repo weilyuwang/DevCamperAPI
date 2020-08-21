e41d661c1138e89416531bacb9667b4ba82b8c33
weilyuwang <weilyuwang@gmail.com>
2020-08-21 12:23:54 -0400

    :fire: average rating

--- models/Review.js
+++ models/Review.js
@@ -33,7 +33,45 @@ const ReviewSchema = new mongoose.Schema({
   },
 });
 
+
+
 // Prevent user from submitting more than one review per bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true })
+
+// Static method to get average rating for a bootcamp and save
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+  // Calculate average rating with aggregate()
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: "$bootcamp",
+        averageRating: { $avg: "$rating" },
+      },
+    },
+  ]);
+
+  // Update Bootcamp model with the new averageCost value
+  try {
+    await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+      averageRating: obj[0].averageRating
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Call getAverageCost after save
+ReviewSchema.post("save", function () {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Call getAverageCost before remvoe
+ReviewSchema.pre("remove", function () {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
 
 module.exports = mongoose.model("Review", ReviewSchema);
